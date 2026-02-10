ceda8bccc611bd03a37e4e0ef45452498e121d36
Sam_intech <sonowosamuel@icloud.com>
2026-02-10 14:09:41 +0000

    backend connected to frontend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 import asyncio
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from qa import run_qa  # you refactor qa.py into a function
 # =======================================================================
 
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:5173"],
+  # allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],  
+)
 
 class Query(BaseModel):
   question: str
@@ -13,8 +25,7 @@ class Query(BaseModel):
 async def ask(query: Query):
   answer = await asyncio.to_thread(run_qa, query.question)
   return {
-    "answer": answer,
-    # "sources": sources
+    "answer": answer
   }
 
 
